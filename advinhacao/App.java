24bf1f087f7d912d8711b2a479308433a1ea6c7a
Andrey Masiero <andreymasiero@gmail.com>
2020-02-20 06:34:15 -0300

    Aula 03 - Condicionais e Laço de Repetição

---
+++ advinhacao/App.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("********************");
+		System.out.println(" Jogo da Advinhacao ");
+		System.out.println("********************");
+
+		int numeroSecreto = 42;
+
+		System.out.printf("O numero secreto eh %d\n", numeroSecreto);
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		int tentativa = 1;
+		int maximoTentativas = 3;
+
+		while(tentativa <= maximoTentativas) {
+			System.out.printf("Tentativa %d de %d.\n", tentativa, maximoTentativas);
+			System.out.print("Informe um numero inteiro (entre 0 e 100): ");
+			int numeroInformado = entrada.nextInt();
+	
+			boolean acertou = numeroSecreto == numeroInformado;
+
+			if(acertou) {
+				System.out.println("Parabens! Voce acertou!");
+				break;
+			} else {
+				System.out.println("Errrrooooooooooou!");			
+			}
+
+			tentativa++;
+		}
+	} 
+}
